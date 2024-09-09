1e169e497ea12f9921790d2f1dd95b398ab2b63e
EZ7-easy <b.sh.tutor@gmail.com>
2024-09-09 22:21:28 +0500

    favicon ico

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -28,7 +28,6 @@ export async function generateStaticParams() {
 export const metadata: Metadata = {
 	title: 'Startup Praktikum - Next.js',
 	description: "Startup Praktikum's Next.js project",
-	icons: { icon: '/logo.svg' },
 }
 
 interface Props extends ChildProps {
