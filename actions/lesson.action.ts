94dde94d018d14b4817f2ded6fb4f52eb9a4dcf7
EZ7-easy <b.sh.tutor@gmail.com>
2024-09-10 22:52:16 +0500

    23-course finished

--- actions/lesson.action.ts
+++ actions/lesson.action.ts
@@ -131,11 +131,11 @@ export const uncompleteLesson = async (lessonId: string, path: string) => {
 	}
 }
 
-// export const getLesson = async (id: string) => {
-// 	try {
-// 		await connectToDatabase()
-// 		return await Lesson.findById(id).select('title content videoUrl')
-// 	} catch (error) {
-// 		throw new Error('Something went wrong!')
-// 	}
-// }
+export const getLesson = async (id: string) => {
+	try {
+		await connectToDatabase()
+		return await Lesson.findById(id).select('title content videoUrl')
+	} catch (error) {
+		throw new Error('Something went wrong!')
+	}
+}
