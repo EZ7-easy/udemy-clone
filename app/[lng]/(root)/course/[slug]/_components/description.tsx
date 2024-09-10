99313621184e09bea9fadf2e5b7ea2c4dd4cbd49
EZ7-easy <b.sh.tutor@gmail.com>
2024-09-10 22:09:49 +0500

    added lesson slug

--- app/[lng]/(root)/course/[slug]/_components/description.tsx
+++ app/[lng]/(root)/course/[slug]/_components/description.tsx
@@ -12,9 +12,33 @@ import {
 } from 'lucide-react'
 import { GrCertificate } from 'react-icons/gr'
 import { BiCategory } from 'react-icons/bi'
+import { useState } from 'react'
+import { useAuth } from '@clerk/nextjs'
+import { purchaseCourse } from '@/actions/course.action'
+import { useParams, useRouter } from 'next/navigation'
+import { toast } from 'sonner'
+import FillLoading from '@/components/shared/fill-loading'
 
 function Description(course: ICourse) {
+	const [isLoading, setIsloading] = useState(false)
+
+	const { userId } = useAuth()
 	const t = useTranslate()
+	const router = useRouter()
+	const { lng } = useParams()
+
+	const onPurchase = async () => {
+		setIsloading(true)
+		const promise = purchaseCourse(course._id, userId!)
+			.then(() => router.push(`/${lng}/dashboard/${course._id}`))
+			.catch(() => setIsloading(false))
+
+		toast.promise(promise, {
+			loading: t('loading'),
+			success: t('successfully'),
+			error: t('error'),
+		})
+	}
 
 	return (
 		<div className='rounded-md border bg-secondary/50 p-4 shadow-lg dark:shadow-white/20 lg:sticky lg:top-24 lg:p-6'>
@@ -36,7 +60,14 @@ function Description(course: ICourse) {
 			<Button size={'lg'} className='mt-4 w-full font-bold'>
 				{t('addToCart')}
 			</Button>
-			<Button size={'lg'} className='mt-2 w-full font-bold' variant={'outline'}>
+			<Button
+				size={'lg'}
+				className='relative mt-2 w-full font-bold'
+				variant={'outline'}
+				onClick={onPurchase}
+				disabled={isLoading}
+			>
+				{isLoading && <FillLoading />}
 				{t('buyNow')}
 			</Button>
 
