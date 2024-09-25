3997a7c1b84158d52c195ae9025d2771ece2430b
EZ7-easy <b.sh.tutor@gmail.com>
2024-09-25 19:35:57 +0500

    course card

--- components/cards/admin-course.card.tsx
+++ components/cards/admin-course.card.tsx
@@ -9,6 +9,7 @@ import { usePathname } from 'next/navigation'
 import { updateCourse } from '@/actions/course.action'
 import { sendNotification } from '@/actions/notification.action'
 import { toast } from 'sonner'
+import { GlareCard } from '../ui/glare-card'
 
 function AdminCourseCard({ course }: { course: ICourse }) {
 	const pathname = usePathname()
@@ -41,48 +42,50 @@ function AdminCourseCard({ course }: { course: ICourse }) {
 	}
 
 	return (
-		<Card className='w-full'>
-			<CardContent className='relative h-56 w-full'>
-				<Image
-					fill
-					src={course.previewImage}
-					alt={course.title}
-					className='rounded-md object-cover'
-				/>
-			</CardContent>
-			<div className='my-4 flex flex-col space-y-2 px-2'>
-				<h2 className='line-clamp-1 font-space-grotesk text-2xl font-bold'>
-					{course.title}
-				</h2>
+		<GlareCard>
+			<Card className='w-full'>
+				<CardContent className='relative h-56 w-full'>
+					<Image
+						fill
+						src={course.previewImage}
+						alt={course.title}
+						className='rounded-md object-cover'
+					/>
+				</CardContent>
+				<div className='my-4 flex flex-col space-y-2 px-2'>
+					<h2 className='line-clamp-1 font-space-grotesk text-2xl font-bold'>
+						{course.title}
+					</h2>
 
-				<Separator />
+					<Separator />
 
-				<div className='flex items-center justify-between'>
-					<div className='flex items-center gap-2'>
-						<Image
-							src={course.instructor.picture}
-							alt={course.instructor.fullName}
-							width={40}
-							height={40}
-							className='rounded-full object-cover'
-						/>
-						<p className='text-sm text-muted-foreground'>
-							{course.instructor.fullName}
-						</p>
+					<div className='flex items-center justify-between'>
+						<div className='flex items-center gap-2'>
+							<Image
+								src={course.instructor.picture}
+								alt={course.instructor.fullName}
+								width={40}
+								height={40}
+								className='rounded-full object-cover'
+							/>
+							<p className='text-sm text-muted-foreground'>
+								{course.instructor.fullName}
+							</p>
+						</div>
+
+						<Button
+							className='w-fit font-space-grotesk font-bold'
+							rounded={'full'}
+							size={'sm'}
+							variant={course.published ? 'destructive' : 'default'}
+							onClick={onToggleStatus}
+						>
+							{course.published ? 'Unpublish' : 'Publish'}
+						</Button>
 					</div>
-
-					<Button
-						className='w-fit font-space-grotesk font-bold'
-						rounded={'full'}
-						size={'sm'}
-						variant={course.published ? 'destructive' : 'default'}
-						onClick={onToggleStatus}
-					>
-						{course.published ? 'Unpublish' : 'Publish'}
-					</Button>
 				</div>
-			</div>
-		</Card>
+			</Card>
+		</GlareCard>
 	)
 }
 
