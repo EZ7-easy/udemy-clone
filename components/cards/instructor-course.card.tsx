e45ccad2bdd5f9e0a8a106052204726810af80b3
EZ7-easy <b.sh.tutor@gmail.com>
2024-09-25 19:44:11 +0500

    commit

--- components/cards/instructor-course.card.tsx
+++ components/cards/instructor-course.card.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import { Badge } from '../ui/badge'
 import { ICourse } from '@/app.types'
 import Link from 'next/link'
-import { GlareCard } from '../ui/glare-card'
 
 interface Props {
 	course: ICourse
@@ -10,28 +9,26 @@ interface Props {
 
 function InstructorCourseCard({ course }: Props) {
 	return (
-		<GlareCard>
-			<Link href={`/en/instructor/my-courses/${course._id}`}>
-				<div className='flex flex-col space-y-2 rounded-md bg-background p-2'>
-					<div className='relative h-52 w-full'>
-						<Image
-							src={course.previewImage}
-							alt={course.title}
-							fill
-							className='rounded-md object-cover'
-						/>
-					</div>
-					<div className='flex items-center justify-between px-2'>
-						<h1 className='font-space-grotesk text-2xl font-bold'>
-							{course.title}
-						</h1>
-						<Badge variant={course.published ? 'default' : 'destructive'}>
-							{course.published ? 'Published' : 'Draft'}
-						</Badge>
-					</div>
+		<Link href={`/en/instructor/my-courses/${course._id}`}>
+			<div className='flex flex-col space-y-2 rounded-md bg-background p-2'>
+				<div className='relative h-52 w-full'>
+					<Image
+						src={course.previewImage}
+						alt={course.title}
+						fill
+						className='rounded-md object-cover'
+					/>
 				</div>
-			</Link>
-		</GlareCard>
+				<div className='flex items-center justify-between px-2'>
+					<h1 className='font-space-grotesk text-2xl font-bold'>
+						{course.title}
+					</h1>
+					<Badge variant={course.published ? 'default' : 'destructive'}>
+						{course.published ? 'Published' : 'Draft'}
+					</Badge>
+				</div>
+			</div>
+		</Link>
 	)
 }
 
