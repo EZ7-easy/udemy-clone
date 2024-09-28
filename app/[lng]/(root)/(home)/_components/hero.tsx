693bb9a5cf187eb37805edf59769dcbafca28793
EZ7-easy <b.sh.tutor@gmail.com>
2024-09-28 20:40:03 +0500

    lazy loading

--- app/[lng]/(root)/(home)/_components/hero.tsx
+++ app/[lng]/(root)/(home)/_components/hero.tsx
@@ -10,6 +10,7 @@ import { companies } from '@/constants'
 import useTranslate from '@/hooks/use-translate'
 import Link from 'next/link'
 import Autoplay from 'embla-carousel-autoplay'
+import { IconCloudDemo } from '@/components/cards/icon-cloud'
 
 function Hero() {
 	const t = useTranslate()
@@ -36,10 +37,10 @@ function Hero() {
 						</Link>
 					</div>
 				</div>
-				{/* 
+
 				<div className='self-center'>
 					<IconCloudDemo />
-				</div> */}
+				</div>
 			</div>
 
 			<div className='w-full bg-secondary'>
--- components/cards/icon-cloud.tsx
+++ components/cards/icon-cloud.tsx
@@ -1,11 +1,15 @@
+'use client'
+
 import IconCloud from '@/components/magicui/icon-cloud'
+import { cn } from '@/lib/utils'
+import { useState } from 'react'
 
 const slugs = [
 	'typescript',
 	'javascript',
 	'dart',
 	'java',
-	'react',
+	'telegram',
 	'flutter',
 	'android',
 	'html5',
@@ -34,8 +38,17 @@ const slugs = [
 ]
 
 export function IconCloudDemo() {
+	const [loading, setLoading] = useState(true)
+
 	return (
-		<div className='relative mx-auto flex size-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg bg-background px-20 pb-20 pt-8 max-lg:w-[25rem] max-md:w-[18rem] max-md:px-0'>
+		<div
+			className={cn(
+				'object-cover duration-700 ease-in-out transition-all scale-100 blur-0 grayscale-0',
+				loading && 'scale-110 blur-2xl grayscale',
+				'relative mx-auto flex size-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg bg-background px-20 pb-20 pt-8 max-lg:w-[25rem] max-md:w-[18rem] max-md:px-0'
+			)}
+			onLoad={() => setLoading(false)}
+		>
 			<IconCloud iconSlugs={slugs} />
 		</div>
 	)
