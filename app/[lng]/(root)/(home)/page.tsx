060a7a1f28cf2f1626cb383c59b35dc294e15b9c
“EZ7-easy” <b.sh.tutor@gmail.com>
2024-12-22 14:09:27 +0500

    added instructors

--- app/[lng]/(root)/(home)/page.tsx
+++ app/[lng]/(root)/(home)/page.tsx
@@ -2,13 +2,13 @@ import { getFeaturedCourses } from "@/actions/course.action";
 import Categories from "./_components/categories";
 import FeaturedCourses from "./_components/featured-courses";
 import Hero from "./_components/hero";
-// import Instructor from "./_components/instructor";
+import Instructor from "./_components/instructor";
 import LearningJourney from "./_components/learning-journey";
-// import { getAdminInstructors } from "@/actions/user.action";
+import { getAdminInstructors } from "@/actions/user.action";
 
 async function Page() {
   const courses = await getFeaturedCourses();
-  //   const instructorData = await getAdminInstructors({ pageSize: 4 });
+  const instructorData = await getAdminInstructors({ pageSize: 4 });
 
   return (
     <>
